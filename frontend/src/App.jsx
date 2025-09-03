6e42116ec00749faed53b7b4d50b6620e74c6265
Simran <133814475+simmidodo@users.noreply.github.com>
2025-09-03 16:42:28 +0100

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1> Full Stack CI/CD on AWS</h1>
+      <p>Frontend: React (Vite)</p>
+      <p>Backend: Node.js/Express</p>
+      <p>Database: MySQL</p>
+    </div>
+  );
+}
+
+export default App;
